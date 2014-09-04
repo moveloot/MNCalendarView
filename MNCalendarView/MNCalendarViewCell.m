d85de9537392709b204b3450e84eaf5b9e082343
Andy Brett <andy@andybrett.com>
2014-09-04 14:45:09 -0700

    change selected cell color

--- MNCalendarView/MNCalendarViewCell.m
+++ MNCalendarView/MNCalendarViewCell.m
@@ -46,7 +46,7 @@ NSString *const MNCalendarViewCellIdentifier = @"MNCalendarViewCellIdentifier";
     
     self.selectedBackgroundView = [[UIView alloc] initWithFrame:self.bounds];
     self.selectedBackgroundView.autoresizingMask = UIViewAutoresizingFlexibleWidth|UIViewAutoresizingFlexibleHeight;
-    self.selectedBackgroundView.backgroundColor = [UIColor colorWithRed:0.23f green:0.61f blue:1.f alpha:1.f];
+    self.selectedBackgroundView.backgroundColor = [UIColor colorWithRed:243.0/255.0f green:82.0f/255.0f blue:81.0/255.0f alpha:1.0f];
   }
   return self;
 }
