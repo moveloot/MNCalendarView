bf131002b4e6101a4635b664a9953206397ea29a
Andy Brett <andy@andybrett.com>
2014-12-13 14:30:18 -0800

    Update MNCalendarViewCell.m

--- MNCalendarView/MNCalendarViewCell.m
+++ MNCalendarView/MNCalendarViewCell.m
@@ -35,7 +35,7 @@ NSString *const MNCalendarViewCellIdentifier = @"MNCalendarViewCellIdentifier";
     
     self.titleLabel = [[UILabel alloc] initWithFrame:self.bounds];
     self.titleLabel.autoresizingMask = UIViewAutoresizingFlexibleWidth|UIViewAutoresizingFlexibleHeight;
-    self.titleLabel.font = [UIFont systemFontOfSize:14.f];
+    self.titleLabel.font = [UIFont fontWithName:@"GillSans" size:19.0f];
     self.titleLabel.textColor = [UIColor darkTextColor];
     self.titleLabel.highlightedTextColor = [UIColor whiteColor];
     self.titleLabel.textAlignment = NSTextAlignmentCenter;
